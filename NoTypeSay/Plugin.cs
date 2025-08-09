461d1327daf71ccef63c2cfca7764df964dd4388
salanth357 <salanth357@gmail.com>
2025-08-09 01:50:34 -0400

    7.3 Refactor
    - Refactor to use the ToDo(Number|String)Array structs for clearer code.
    - Properly dispose of our copies of the Utf8Strings when the UI updates
    - Remove that signature and more gracefully hook the ReceiveEvent vf
    - Run style cleanup on the code, sorry PAC

--- NoTypeSay/Plugin.cs
+++ NoTypeSay/Plugin.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using Dalamud.IoC;
-using Dalamud.Plugin;
 using System.Linq;
 using System.Text;
 using Dalamud.Game;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Hooking;
+using Dalamud.IoC;
+using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
-using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Arrays;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel;
 using Lumina.Excel.Sheets;
@@ -21,27 +21,20 @@ namespace NoTypeSay;
 // ReSharper disable once UnusedType.Global
 public sealed unsafe class Plugin : IDalamudPlugin
 {
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
-    [PluginService] internal static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
-    [PluginService] internal static IPluginLog PluginLog { get; private set; } = null!;
-    [PluginService] internal static IGameInteropProvider GameInteropProvider { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IFramework Framework { get; private set; } = null!;
-    
     // This is the ID for eat chicken plus the flag they use to indicate an emote ID
     // We use this as a sentinel for identifying a button we injected, since we don't
     // expect them to ever require a /eatchicken
     private const int EmoteActionId = 0x2000000 | 271;
-    
+
     // This is the icon ID for the button
     private const int ConverseIconId = 64345;
 
+    private readonly List<Entry> questEntries = [];
+
+    private Hook<AtkEventListener.Delegates.ReceiveEvent>? receiveEventDetour;
+
     public Plugin()
     {
-        GameInteropProvider.InitializeFromAttributes(this);
-
-        receiveEventDetour?.Enable();
-        
         AddonLifecycle.RegisterListener(AddonEvent.PreRequestedUpdate, "_ToDoList", PreReqUpdate);
 
         if (ClientState.IsLoggedIn)
@@ -52,6 +45,25 @@ public sealed unsafe class Plugin : IDalamudPlugin
         }
     }
 
+    [PluginService]
+    internal static IDataManager DataManager { get; private set; } = null!;
+
+    [PluginService]
+    internal static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
+
+    [PluginService]
+    internal static IPluginLog PluginLog { get; private set; } = null!;
+
+    [PluginService]
+    internal static IGameInteropProvider GameInteropProvider { get; private set; } = null!;
+
+    [PluginService]
+    internal static IClientState ClientState { get; private set; } = null!;
+
+    [PluginService]
+    internal static IFramework Framework { get; private set; } = null!;
+
+    private static StringArrayData* StringArray => AtkStage.Instance()->GetStringArrayData(StringArrayType.ToDoList);
 
     public void Dispose()
     {
@@ -63,111 +75,62 @@ public sealed unsafe class Plugin : IDalamudPlugin
         // We should avoid modifying these from arbitrary threads, so clean ourselves up on the main thread
         Framework.Run(() =>
         {
-            var nad = RaptureAtkModule.Instance()->GetNumberArrayData((int)NumberArrayType.ToDoList);
-            var sad = RaptureAtkModule.Instance()->GetStringArrayData((int)StringArrayType.ToDoList);
+            var numberArray = ToDoListNumberArray.Instance();
+            var sad = StringArray;
 
             var offset = 0;
-            foreach (var e in questEntries)
-            {
-                e.ResetTodoEntry(nad, sad, ref offset);
-            }
+            foreach (var e in questEntries) e.ResetTodoEntry(numberArray, sad, ref offset);
             ForceRedraw();
         });
     }
 
     private static void ForceRedraw()
     {
-        var nad = RaptureAtkModule.Instance()->AtkArrayDataHolder.GetNumberArrayData((int)NumberArrayType.ToDoList);
-        // We aren't changing anything, but this will force the appropriate redraw to happen
-        nad->SetValue(7, nad->IntArray[7], true);
+        StringArray->UpdateState = 1;
     }
-
-    private class Entry
-    {
-        internal int Index;
-        internal bool Modified;
-        internal string Message = "";
-
-        internal Utf8String *TitleText; // used to id the quest
-        internal Utf8String *DetailText; // used to id the quest step
-        
-        // These should never have more than 4 items
-        internal int ButtonCount;
-        internal readonly List<int> ItemIDs = [];
-        internal readonly List<int> IconIDs = [];
-        internal Utf8String*[] Strings = [];
-        
-        // ReSharper disable once UnusedMember.Local
-        public new string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.Append($"{Index}: [{(Modified ? 'x' : ' ')}] {ButtonCount} {TitleText->ToString()}");
-            return sb.ToString();
-        }
-
-        internal void ResetTodoEntry(NumberArrayData* nad, StringArrayData* sad, ref int offset)
-        {
-            // If we modified it AND we have buttons to remove,
-            // decrement the button count (we're always the last one)
-            if (Modified && ButtonCount > 0)
-            {
-                ButtonCount--;
-                Modified = false;
-            }
-            WriteTodoEntry(nad, sad, ref offset);
-            
-        }
-
-        internal void WriteTodoEntry(NumberArrayData* nad, StringArrayData* sad, ref int offset)
-        {
-            nad->IntArray[Index + 39] = ButtonCount;
-            for (var i = 0; i < ButtonCount; i++)
-            {
-                // Don't propagate the update here since we'll be making a lot of changes
-                // Also, we're either already _about_ to process an update or we'll request
-                // one when we're done
-                nad->IntArray[offset+49] = ItemIDs[i];
-                nad->IntArray[offset+89] = IconIDs[i];
-                sad->SetValueUtf8(offset + 119, Strings[i], suppressUpdates:true);
-                offset++;
-            }
-        }
-    }
-
-    private readonly List<Entry> questEntries = [];
 
     private void PreReqUpdate(AddonEvent type, AddonArgs addonArgs)
     {
-        var args = (AddonRequestedUpdateArgs)addonArgs;
-        ProcessUpdate((NumberArrayData**)args.NumberArrayData, (StringArrayData**)args.StringArrayData);
+        // Lazily load this hook so we can use the vtable instead of a sig
+        if (receiveEventDetour == null)
+        {
+            var todoList = (AddonToDoList*)addonArgs.Addon.Address;
+            receiveEventDetour =
+                GameInteropProvider.HookFromAddress<AtkEventListener.Delegates.ReceiveEvent>(
+                    (nint)todoList->VirtualTable->ReceiveEvent, ReceiveEventDetour);
+            receiveEventDetour?.Enable();
+        }
+
+        ProcessUpdate();
     }
 
-    private void ProcessUpdate(NumberArrayData** numbers, StringArrayData** strings)
+    private void ProcessUpdate()
     {
-        var nad = numbers[(int)NumberArrayType.ToDoList];
-        var sad = strings[(int)StringArrayType.ToDoList];
+        var numArray = ToDoListNumberArray.Instance();
+        var stringArray = ToDoListStringArray.Instance();
 
         // UI disabled, just leave
-        if (nad->IntArray[7] == 0) return;
+        if (!numArray->QuestListEnabled) return;
 
         var offset = 0;
+        foreach (var e in questEntries) e.Dispose();
         questEntries.Clear();
-        var entryCount = nad->IntArray[8];
+        var entryCount = numArray->QuestCount;
         for (var i = 0; i < entryCount; i++)
         {
             var e = new Entry
             {
                 Index = i,
-                TitleText = Utf8String.FromSequence((byte*)sad->StringArray[9 + i]),
-                DetailText = Utf8String.FromSequence((byte*)sad->StringArray[9 + entryCount + i]),
-                ButtonCount = nad->IntArray[39 + i]
+                TitleText = Utf8String.FromSequence((byte*)stringArray->QuestTexts[i]),
+                DetailText = Utf8String.FromSequence((byte*)stringArray->QuestTexts[entryCount + i]),
+                ButtonCount = numArray->ButtonCountForQuest[i]
             };
             e.Strings = new Utf8String*[e.ButtonCount + 1]; // Always make room for one more in case I need to add
             for (var j = 0; j < e.ButtonCount; j++)
             {
-                e.ItemIDs.Add(nad->IntArray[49 + offset]);
-                e.IconIDs.Add(nad->IntArray[89 + offset]);
-                e.Strings[j] = Utf8String.FromSequence((byte*)sad->StringArray[119 + offset]);
+                e.ItemIDs.Add(numArray->QuestButtonActionId[offset]);
+                e.IconIDs.Add(numArray->QuestButtonIconID[offset]);
+                e.Strings[j] = Utf8String.FromSequence((byte*)stringArray->QuestStatusMessages[offset]);
                 offset++;
             }
 
@@ -192,29 +155,23 @@ public sealed unsafe class Plugin : IDalamudPlugin
             questEntries.Add(e);
         }
 
+        if (questEntries.Count == 0) return;
+        var sad = StringArray;
         offset = 0;
-        foreach (var e in questEntries)
-        {
-            e.WriteTodoEntry(nad, sad, ref offset);
-        }
+        foreach (var e in questEntries) e.WriteTodoEntry(numArray, sad, ref offset);
     }
 
-    // Once AddonToDoList is added to ClientStructs, this can be replaced with a vtable hook,
-    // but for now we'll use this probably-longer-than-necessary signature
-    // ReSharper disable once FieldCanBeMadeReadOnly.Local
-    [Signature("48 89 5C 24 ?? 55 56 57 41 56 41 57 48 8B EC 48 83 EC 70 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 F0 41 8B F8", DetourName = nameof(ReceiveEventDetour))]
-    private Hook<AtkEventListener.Delegates.ReceiveEvent>? receiveEventDetour = null!;
 
-    
-    private void ReceiveEventDetour(AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData)
+    private void ReceiveEventDetour(
+        AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent,
+        AtkEventData* atkEventData)
     {
         if (!HandleEvent(eventType, eventParam, atkEvent, atkEventData))
-        {
             receiveEventDetour?.Original.Invoke(thisPtr, eventType, eventParam, atkEvent, atkEventData);
-        }
     }
-    
-    private bool HandleEvent(AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData) {
+
+    private bool HandleEvent(AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData)
+    {
         try
         {
             if (eventType != AtkEventType.MouseUp || eventParam != 1 || atkEventData->MouseData.ButtonId == 1)
@@ -254,8 +211,8 @@ public sealed unsafe class Plugin : IDalamudPlugin
             // Now let's make sure we have the right button - we almost certainly do, since the button
             // is almost definitely the only one present on the entry, but let's be safe.
             // Yes, they store the event item/emote ID as the event's node pointer.
-            var itemOrEmoteId = *(UInt32*)atkEvent->Node;
-            if (itemOrEmoteId != (0x2000000 | 271))
+            var itemOrEmoteId = *(uint*)atkEvent->Node;
+            if (itemOrEmoteId != EmoteActionId)
             {
                 PluginLog.Debug("not our button");
                 return false;
@@ -274,16 +231,19 @@ public sealed unsafe class Plugin : IDalamudPlugin
             return true;
         }
     }
+
     private static void SendChat(string message)
     {
         var str = Utf8String.FromString(message);
-        str->SanitizeString(AllowedEntities.Numbers|AllowedEntities.UppercaseLetters|AllowedEntities.LowercaseLetters|AllowedEntities.OtherCharacters|AllowedEntities.SpecialCharacters);
+        str->SanitizeString(AllowedEntities.Numbers | AllowedEntities.UppercaseLetters |
+                            AllowedEntities.LowercaseLetters | AllowedEntities.OtherCharacters |
+                            AllowedEntities.SpecialCharacters);
         // This is the max length the chatbox would allow someone to type in
         // so make sure we don't send anything longer than this
         if (str->StringLength > 500) return;
         UIModule.Instance()->ProcessChatBoxEntry(str);
     }
-    
+
     // While we _could_ just keep a list of all /say quests, unless they
     // completely change the way they implement them, there's always a QuestDialogue
     // that contains _just_ the expected message, so we'll just look that up.
@@ -300,7 +260,7 @@ public sealed unsafe class Plugin : IDalamudPlugin
     {
         if (todoText == message) return false; // we don't want to match against the current todo step
         return ClientState.ClientLanguage switch
-        {       
+        {
             ClientLanguage.English => todoText.Contains($"“{message}”"),
             ClientLanguage.French => todoText.Contains($"“{message}”"),
             ClientLanguage.Japanese => todoText.Contains($"『{message}』") || todoText.Contains($"「{message}」"),
@@ -308,11 +268,72 @@ public sealed unsafe class Plugin : IDalamudPlugin
             _ => false
         };
     }
-    
+
     private static ExcelSheet<QuestDialogue> TextSheetForQuest(Quest q)
     {
         var qid = q.Id.ToString();
         var dir = qid.Substring(qid.Length - 5, 3);
         return DataManager.GetExcelSheet<QuestDialogue>(name: $"quest/{dir}/{qid}");
+    }
+
+    private class Entry : IDisposable
+    {
+        internal readonly List<int> IconIDs = [];
+        internal readonly List<int> ItemIDs = [];
+
+        // These should never have more than 4 items
+        internal int ButtonCount;
+        internal Utf8String* DetailText; // used to id the quest step
+        internal int Index;
+        internal string Message = "";
+        internal bool Modified;
+        internal Utf8String*[] Strings = [];
+
+        internal Utf8String* TitleText; // used to id the quest
+
+        public void Dispose()
+        {
+            if (TitleText != null) TitleText->Dtor(true);
+            if (DetailText != null) TitleText->Dtor(true);
+            foreach (var s in Strings)
+                if (s != null)
+                    s->Dtor(true);
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        public new string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{Index}: [{(Modified ? 'x' : ' ')}] {ButtonCount} {TitleText->ToString()}");
+            return sb.ToString();
+        }
+
+        internal void ResetTodoEntry(ToDoListNumberArray* nad, StringArrayData* sad, ref int offset)
+        {
+            // If we modified it AND we have buttons to remove,
+            // decrement the button count (we're always the last one)
+            if (Modified && ButtonCount > 0)
+            {
+                ButtonCount--;
+                Modified = false;
+            }
+
+            WriteTodoEntry(nad, sad, ref offset);
+        }
+
+        internal void WriteTodoEntry(ToDoListNumberArray* nad, StringArrayData* sad, ref int offset)
+        {
+            nad->ButtonCountForQuest[Index] = ButtonCount;
+            for (var i = 0; i < ButtonCount; i++)
+            {
+                // Don't propagate the update here since we'll be making a lot of changes
+                // Also, we're either already _about_ to process an update or we'll request
+                // one when we're done
+                nad->QuestButtonActionId[offset] = ItemIDs[i];
+                nad->QuestButtonIconID[offset] = IconIDs[i];
+                sad->SetValueUtf8(offset + 119, Strings[i], suppressUpdates: true);
+                offset++;
+            }
+        }
     }
 }
